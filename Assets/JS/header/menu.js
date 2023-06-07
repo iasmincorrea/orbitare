4156390ce2f92c71de9c57a84310f75161338048
iasmincorrea <iasmin_correa@hotmail.com>
2023-06-07 11:52:01 -0300

    ajustes pagespeed

--- Assets/JS/header/menu.js
+++ Assets/JS/header/menu.js
@@ -1,34 +1,27 @@
 if (window.matchMedia("(max-width:1025px)").matches) {
 
   function openMenu() {
-    if(document.querySelector(".menu").classList.contains("menu-open")) {
-      document.querySelector(".menu").classList.remove("menu-open");
-    } else {
-      document.querySelector(".menu").classList.add("menu-open");
-    }
-    document.querySelector(".header").classList.toggle("header-open");
+    var menuMobile = document.querySelector('#menu-mobile');
+    var header = document.querySelector('.header');
+  
+    menuMobile.classList.toggle('menu-open');
+    header.classList.toggle('header-open');
   }
-    
-  // Mobile - Fechar menu e direcionar para a âncora
-  var links = document.querySelectorAll('a[href^="#"]');
+  
+  var menuItems = document.querySelectorAll('.menu-item__title');
 
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function(event) {
-
-        var menuMobile = document.querySelector('#menu-mobile');
-        var header = document.querySelector('.header');
-
-        if (menuMobile.classList.contains('menu-open')) {
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function(event) {
+      var menuMobile = document.querySelector('#menu-mobile');
+      var header = document.querySelector('.header');
+      var menuButton = document.querySelector('.header-menu');
+  
+      if (menuMobile.classList.contains('menu-open')) {
         menuMobile.classList.remove('menu-open');
         header.classList.remove('header-open');
-        }
-
-        event.preventDefault();
-        var target = document.querySelector(this.getAttribute('href'));
-        window.scrollTo({
-        top: target.offsetTop,
-        behavior: 'smooth'
-        });
+        menuButton.classList.remove('menu-image__open');
+        menuButton.classList.add('menu-image__closed');
+      }
     });
   }
   
